c239b8c0d2ebf9fa6a033a1b6d61be53cbf284ab
ShamusDuffey <shamusduffey@gmail.com>
2024-09-05 00:00:51 -0400

    Update numberFilter.c

--- Program 3/numberFilter.c
+++ Program 3/numberFilter.c
@@ -1,1 +1,23 @@
-ECHO is on.
+/**********************************************************************
+Program: Number Filter
+Author: Shamus Duffey
+Date: <date you finish the program>
+Time spent: <total amount of time spent on the project>
+Purpose: The purpose of this program is to blah blah blah
+***********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Bitflags.h"
+
+int main(int argc, char* argv[])//index is currently a negative value sometimes
+{
+	BIT_FLAGS storage = bit_flags_init_number_of_bits(2000000000);
+	printf("Start: %d\n", bit_flags_check_flag(storage, 1999999999));
+	bit_flags_set_flag(storage, 999999999);
+	printf("End: %d\n", bit_flags_check_flag(storage, 1999999999));
+	bit_flags_unset_flag(storage, 999999999);
+	printf("After unsetting: %d\n", bit_flags_check_flag(storage, 1999999999));
+	bit_flags_destroy(&storage);
+	return 0;
+}
